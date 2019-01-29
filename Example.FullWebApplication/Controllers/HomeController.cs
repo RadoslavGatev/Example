b577f1a0d09323286c6f2fcf48f4169e26526d64
Radoslav Gatev <radoslavgatev@gmail.com>
2019-01-29 22:09:12 +0200

    Add ASP.NET MVC Web App.

---
+++ Example.FullWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Example.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.FullWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repo = new Repo();
+            return View(model: repo.Get());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
